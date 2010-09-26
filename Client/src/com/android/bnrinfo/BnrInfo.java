d827d5d3b625388f3f47f6311737b60470b379d4
adrian.siminiceanu <adrian.siminiceanu@3702e458-f199-0b41-2bf2-a731bec3642e>
2010-09-26 17:44:00 +0000

    first commit

---
+++ Client/src/com/android/bnrinfo/BnrInfo.java
@@ -0,0 +1,358 @@
+package com.android.bnrinfo;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.telephony.TelephonyManager;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class BnrInfo extends Activity {
+
+    /*
+     * The goodies
+     */
+    private String httpResponse = new String("");
+
+    /*
+     * The error message
+     */
+    private String errorMessage = new String("");
+
+    /*
+     * The app version number
+     */
+    static String appVersion = new String("");
+
+    /*
+     * Need handler for call backs to the UI thread
+     */
+    final Handler uiHandler = new Handler();
+
+    /*
+     * The list of rates types
+     */
+    static ArrayList<String> ratesType = new ArrayList<String>();
+
+    /*
+     * The list of rates multiplier
+     */
+    static ArrayList<String> ratesMultiplier = new ArrayList<String>();
+
+    /*
+     * The list of rates values
+     */
+    static ArrayList<String> ratesValues = new ArrayList<String>();
+
+    /*
+     * The list of rates diff
+     */
+    static ArrayList<String> ratesDiff = new ArrayList<String>();
+
+    /*
+     * The ListView in main layout
+     */
+    private ListView listView;
+
+    /*
+     * The date of the goodies
+     */
+    static String sendingDate = new String("");
+
+    /*
+     * The header TextView
+     */
+    private TextView header;
+
+    /*
+     * Create runnable for posting signals to UI
+     */
+    final Runnable signalUi = new Runnable() {
+        /*
+         * (non-Javadoc)
+         * 
+         * @see java.lang.Runnable#run()
+         */
+        public void run() {
+            updateUi();
+        }
+    };
+
+    /*
+     * Called when the activity is first created.(non-Javadoc)
+     * 
+     * @see android.app.Activity#onCreate(android.os.Bundle)
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Check if already launched the application
+        if (ratesType.size() > 0) {
+            updateUi();
+            return;
+        }
+
+        // Get the data
+        getGoodies();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        // Save UI state changes to the savedInstanceState.
+        // This bundle will be passed to onCreate if the process is
+        // killed and restarted.
+        savedInstanceState.putStringArrayList("ratesType", ratesType);
+        savedInstanceState.putStringArrayList("ratesDiff", ratesDiff);
+        savedInstanceState.putStringArrayList("ratesMultiplier", ratesMultiplier);
+        savedInstanceState.putStringArrayList("ratesValues", ratesValues);
+        savedInstanceState.putString("appVersion", appVersion);
+        savedInstanceState.putString("sendingDate", sendingDate);
+
+        // etc.
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        // Restore UI state from the savedInstanceState.
+        // This bundle has also been passed to onCreate.
+        ratesType = savedInstanceState.getStringArrayList("ratesType");
+        ratesDiff = savedInstanceState.getStringArrayList("ratesDiff");
+        ratesMultiplier = savedInstanceState.getStringArrayList("ratesMultiplier");
+        ratesValues = savedInstanceState.getStringArrayList("ratesValues");
+        appVersion = savedInstanceState.getString("appVersion");
+        sendingDate = savedInstanceState.getString("sendingDate");
+    }
+
+    /*
+     * Execute the HTTP request to get the goodies
+     */
+    protected void executeRequest() {
+        // Create a new HttpClient and Post Header
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpPost httppost = new HttpPost(getString(R.string.serverUrl));
+
+        try {
+            // Add the device id for statistics data
+            TelephonyManager tManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+            String uid = tManager.getDeviceId();
+
+            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(1);
+            nameValuePairs.add(new BasicNameValuePair("DeviceId", uid.toString()));
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+            // Execute HTTP Post Request
+            HttpResponse response = httpclient.execute(httppost);
+
+            HttpEntity resEntity = response.getEntity();
+            httpResponse = EntityUtils.toString(resEntity);
+        } catch (ClientProtocolException e) {
+            errorMessage = new String(getString(R.string.serverErr));
+        } catch (IOException e) {
+            errorMessage = new String(getString(R.string.serverErr));
+        }
+    }
+
+    /*
+     * Parse the JSON response
+     */
+    protected void parseResponse() {
+        try {
+            JSONObject jObject = new JSONObject(httpResponse);
+            JSONArray ratesArray = jObject.getJSONArray("Rates");
+
+            sendingDate = new String(jObject.getString("SendingDate").toString());
+
+            for (int i = 0; i < ratesArray.length(); i++) {
+                ratesType.add(ratesArray.getJSONObject(i).getString("currency").toString());
+                ratesMultiplier.add(ratesArray.getJSONObject(i).getString("multiplier").toString());
+                ratesValues.add(ratesArray.getJSONObject(i).getString("rate").toString());
+                ratesDiff.add(ratesArray.getJSONObject(i).getString("diff").toString());
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+            errorMessage = new String(getString(R.string.serverErr));
+        }
+    }
+
+    /*
+     * Update the UI with the retrieved goodies
+     */
+    protected void updateUi() {
+
+        if (errorMessage.compareTo(new String(getString(R.string.serverErr))) == 0) {
+            AlertDialog errorDialog = new AlertDialog.Builder(this).setIcon(R.drawable.chart)
+                    .setTitle(getString(R.string.app_name) + " " + appVersion).setMessage(errorMessage.toString())
+                    .setNeutralButton(R.string.app_about_button_ok, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int whichButton) {
+                            dialog.dismiss();
+                        }
+                    }).create();
+
+            errorDialog.show();
+            return;
+        }
+
+        setContentView(R.layout.main);
+
+        header = (TextView) findViewById(R.id.header);
+        header.setText(getString(R.string.header) + " " + sendingDate.toString());
+
+        listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(new DisplayAdapter(this));
+    }
+
+    /*
+     * Open the BNR web site in the browser
+     */
+    protected void openBrowser() {
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(getString(R.string.webUrl)));
+        startActivity(i);
+    }
+
+    /*
+     * Open the about dialog
+     */
+    protected void openAbout() {
+        AlertDialog aboutDialog = new AlertDialog.Builder(this).setIcon(R.drawable.chart)
+                .setTitle(getString(R.string.app_name) + " " + appVersion).setMessage(R.string.app_about)
+                .setPositiveButton(R.string.app_about_button_ok, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        dialog.dismiss();
+                    }
+                }).setNegativeButton(R.string.app_about_button_feedback, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        // Send the email with the feedback
+                        Intent i = new Intent(Intent.ACTION_SEND);
+
+                        i.setType("message/rfc822");
+                        i.putExtra(Intent.EXTRA_EMAIL, new String[] { getString(R.string.email_address) });
+                        i.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.email_subject));
+
+                        startActivity(Intent.createChooser(i, getString(R.string.email_select_app)));
+                    }
+                }).create();
+
+        aboutDialog.show();
+    }
+
+    /**
+     * Execute http request and update ui
+     */
+    protected void getGoodies() {
+        // Reset the stored data
+        ratesType.clear();
+        ratesDiff.clear();
+        ratesMultiplier.clear();
+        ratesValues.clear();
+
+        // Get the app version number and store it
+        PackageInfo packageInfo;
+
+        try {
+            packageInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+            appVersion = packageInfo.versionName;
+        } catch (NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        // Create the loading dialog
+        // Retrieving the goodies might take some time
+        final ProgressDialog dialog = ProgressDialog.show(this, getString(R.string.loading_title),
+                getString(R.string.loading_desc), true);
+
+        // Run the HTTP request in a thread so that the dialog will appear
+        Thread httpThread = new Thread(new Runnable() {
+            /*
+             * (non-Javadoc)
+             * 
+             * @see java.lang.Runnable#run()
+             */
+            public void run() {
+                executeRequest();
+                parseResponse();
+                dialog.dismiss();
+                uiHandler.post(signalUi);
+            }
+        });
+
+        httpThread.start();
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see android.app.Activity#onCreateOptionsMenu(android.view.Menu)
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.bnrinfo, menu);
+
+        return true;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see android.app.Activity#onOptionsItemSelected(android.view.MenuItem)
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.about:
+                // About myself
+                openAbout();
+                break;
+
+            case R.id.bnrurl:
+                // Open the browser
+                openBrowser();
+                break;
+
+            case R.id.refresh:
+                // Get the goodies
+                getGoodies();
+                break;
+
+            case R.id.exit:
+                // Hide in the dark
+                finish();
+                break;
+        }
+
+        return true;
+    }
+}
---
+++ Client/src/com/android/bnrinfo/DisplayAdapter.java
@@ -0,0 +1,137 @@
+package com.android.bnrinfo;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class DisplayAdapter extends BaseAdapter {
+
+    /*
+     * The context inflater
+     */
+    private LayoutInflater mInflater;
+
+    /*
+     * Application context
+     */
+    Context mcontext;
+
+    /**
+     * The display adapter constructor
+     * 
+     * @param context
+     */
+    public DisplayAdapter(Context context) {
+        // Cache the LayoutInflate to avoid asking for a new one each time.
+        mInflater = LayoutInflater.from(context);
+        // Save the context
+        mcontext = context;
+    }
+
+    /**
+     * The number of items in the list is determined by the number of speeches
+     * in our array.
+     * 
+     * @see android.widget.ListAdapter#getCount()
+     */
+    public int getCount() {
+        return BnrInfo.ratesType.size();
+    }
+
+    /**
+     * Since the data comes from an array, just returning the index is
+     * sufficient to get at the data. If we were using a more complex data
+     * structure, we would return whatever object represents one row in the
+     * list.
+     * 
+     * @see android.widget.ListAdapter#getItem(int)
+     */
+    public Object getItem(int position) {
+        return position;
+    }
+
+    /**
+     * Use the array index as a unique id.
+     * 
+     * @see android.widget.ListAdapter#getItemId(int)
+     */
+    public long getItemId(int position) {
+        return position;
+    }
+
+    /**
+     * Make a view to hold each row.
+     * 
+     * @see android.widget.ListAdapter#getView(int, android.view.View,
+     *      android.view.ViewGroup)
+     */
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // A ViewHolder keeps references to children views to avoid unnecessary
+        // calls
+        // to findViewById() on each row.
+        ViewHolder holder;
+
+        // When convertView is not null, we can reuse it directly, there is no
+        // need
+        // to re-inflate it. We only inflate a new View when the convertView
+        // supplied
+        // by ListView is null.
+        if (convertView == null) {
+            convertView = mInflater.inflate(R.layout.list_item, null);
+
+            // Creates a ViewHolder and store references to the two children
+            // views
+            // we want to bind data to.
+            holder = new ViewHolder();
+            holder.rateType = (TextView) convertView.findViewById(R.id.rate_type);
+            holder.rateValue = (TextView) convertView.findViewById(R.id.rate_value);
+            holder.rateDiff = (TextView) convertView.findViewById(R.id.rate_diff);
+            holder.rateDesc = (TextView) convertView.findViewById(R.id.rate_desc);
+            holder.icon = (ImageView) convertView.findViewById(R.id.icon);
+
+            convertView.setTag(holder);
+        } else {
+            // Get the ViewHolder back to get fast access to the TextView
+            // and the ImageView.
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        // Bind the data efficiently with the holder.
+        holder.rateType.setText(BnrInfo.ratesMultiplier.get(position) + " " + BnrInfo.ratesType.get(position));
+        holder.rateValue.setText(BnrInfo.ratesValues.get(position) + " RON");
+
+        String diffText = new String(BnrInfo.ratesDiff.get(position));
+        holder.rateDiff.setText(diffText.toString());
+
+        if (diffText.startsWith("+")) {
+            holder.rateDiff.setTextColor(mcontext.getResources().getColor(R.color.color_plus));
+        } else {
+            holder.rateDiff.setTextColor(mcontext.getResources().getColor(R.color.color_minus));
+        }
+
+        holder.rateDesc.setText(mcontext.getString(mcontext.getResources().getIdentifier(
+                "desc_" + BnrInfo.ratesType.get(position), "string", "com.android.bnrinfo")));
+
+        Bitmap bm = BitmapFactory.decodeResource(
+                mcontext.getResources(),
+                mcontext.getResources().getIdentifier("ico_" + BnrInfo.ratesType.get(position).toLowerCase(),
+                        "drawable", "com.android.bnrinfo"));
+        holder.icon.setImageBitmap(bm);
+
+        return convertView;
+    }
+
+    static class ViewHolder {
+        TextView rateType;
+        TextView rateValue;
+        TextView rateDiff;
+        TextView rateDesc;
+        ImageView icon;
+    }
+}
